9d474e237b50fefcce68394e63772316044f85a9
artfrc <80659617+artfrc@users.noreply.github.com>
2021-04-10 11:14:58 -0300

    Add files via upload
    Código do Exercício 1 do Trabalho de AED
    Inserção Não Ordenada de Elementos Float

---
+++ 1/function.c
@@ -0,0 +1,191 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function.h"
+#define MAX 20
+
+struct REAIS{
+
+  float num[MAX];
+  int Fim;
+
+};
+
+Reais cria_lista(){
+
+    Reais p;
+    p = (Reais)malloc(sizeof(struct REAIS));
+
+    if(p != NULL) p->Fim = 0;
+
+    return p;
+
+}
+
+int lista_vazia(Reais p){
+
+    if(p == NULL || p->Fim > 0) return 0;
+    else                        return 1;
+
+}
+
+int lista_cheia(Reais p){
+
+    if(p == NULL || p->Fim != MAX) return 0;
+    else                           return 1;
+
+}
+
+int insere_elem(Reais p, float N){
+
+    if(p == NULL || lista_cheia(p) == 1) return 0;
+
+/* SE A LISTA NAO ESTIVER CHEIA, SIMPLESMENTE VAI INSERIR O ELEMENTO NA
+    POSICAO FINAL E INCREMENTARÁ 1 EM 'FIM' PARA DIZER QUE FOI ADICIONADO UM ELEMENTO */
+
+    p->num[p->Fim] = N;
+    p->Fim++;
+
+    return 1;
+
+}
+
+int remove_elem(Reais p, float N){
+
+    if(p == NULL || lista_vazia(p) == 1) return 0;
+
+
+// SE O ELEMENTO QUE DESEJA SER REMOVIDO FOR O ULTIMO DA LISTA, SÓ DA UM FIM -1
+// PARA DIZER QUE AQUELE ELEMENTO NAO PERTENCE MAIS A LISTA
+    else if(p->num[p->Fim-1] == N){p->Fim--; return 1;}
+
+//SE NAO FOR O ULTIMO, ELE PROCURA ATE ACHAR OU NAO O ELEMENTO
+    else{
+        int i;
+        for(i = 0; i < p->Fim; i++){
+            if(p->num[i] == N) break; //SE ACHOU, ENTAO PAUSA E IREMOS REMOVE-LO ADIANTE
+        }
+
+        if(i == p->Fim) return 0; //SE NAO ACHOU, RETORNA ZERO
+
+        else{ //REMOVER ELEMENTO ENCONTRADO
+            for(i += 1; i < p->Fim; i++){ //COMECO NA POSICAO DO ELEMENTO + 1
+                p->num[i-1] = p->num[i];//PEGO ESSE ELEMENTO E COLOCO NA POSICAO ANTERIOR
+            }                           //FACO ISSO ATE O FINAL
+            p->Fim--;               //E DOU UM FIM - 1 PARA DIZER QUE REMOVI UM ELEMENTO
+            return 1;
+        }
+    }
+
+}
+
+int get_pos(Reais p, int pos, float *elem){
+
+    if(p == NULL || lista_vazia(p) == 1 || p->Fim <= pos) return 0;
+
+    //JA FACO A VERIFICACAO ANTES SE A POSICAO SOLICITADA EH VALIDA
+    //SE FOR, SO PEGO O ELEMENTO DAQUELA POSICAO E PASSO POR REFERENCIA
+    *elem = p->num[pos];
+
+    return 1;
+
+}
+
+int esvazia_lista(Reais p){
+
+    if(p == NULL) return 0;
+
+    //SIMPLESMENTE COLOCO FIM = 0 PARA DIZER QUE A LISTA ESTA VAZIA
+    //O QUE FICOU LA SERA LIXO
+
+    p->Fim = 0;
+
+    return 1;
+
+}
+
+int remove_impares(Reais p){
+
+    if(p == NULL || lista_vazia(p) == 1) return 0;
+
+    int i, aux;
+    for(i = 0; i < p->Fim; i++){
+        aux = p->num[i] * 100; //MULTIPLICACAO NECESSARIA PARA VERIFICAR A PARTE DECIMAL
+        if(aux % 2 != 0){      //SEM NECESSIDADE DE USAR FMOD
+            remove_elem(p, p->num[i]);
+            i--; //COMANDO NECESSARIO, JA QUE IREI REMOVER O ELEMENTO SE FOR IMPAR, E O PROXIMO Q IREI VERIFICAR
+        }       //IRA VIR PARA ESSA POSICAO. SEM ESSE COMANDO, EU NAO FARIA A VERIFICACAO DESSE PROXIMO ELEMENTO
+    }
+
+    return 1;
+
+}
+
+int maior_elemento(Reais p, float *elem){
+
+    if(p == NULL || lista_vazia(p) == 1) return 0;
+
+    int i;
+    *elem = p->num[0]; //COLOCO PARA O PRIMEIRO ELEMENTO SER O MAIOR
+    for(i = 1; i < p->Fim; i++){//DPS VOU FAZENDO A VERIFICACAO SE EXISTE OUTRO MAIOR
+        if(p->num[i] > *elem) *elem = p->num[i];
+    }
+    //O MAIOR ELEMENTO EH PASSADO POR REFERENCIA
+
+    return 1;
+
+}
+
+int tamanho_lista(Reais p, float *tam){
+
+    if(p == NULL) return 0;
+
+    *tam = p->Fim;
+
+    return 1;
+
+}
+
+int lista_invertida(Reais p, Reais p2){
+
+    if(p == NULL || p2 == NULL || lista_vazia(p) == 1) return 0;
+
+    int i;
+    for(i = p->Fim - 1; i >= 0; i--){//INCREMENTO DE TRAS PARA FRENTE A LISTA P NA LISTA P2
+        insere_elem(p2, p->num[i]);
+    }
+
+    return 1;
+
+}
+
+int concatena_listas(Reais p, Reais p2, Reais p3){
+
+    if(p == NULL || p2 == NULL || p3 == NULL) return 0;
+
+    float tamP, tamP2;
+    tamanho_lista(p, &tamP);//A SOMA DO TAMANHO DAS 2 LISTAS PRECISA SER NO MAXIMO 20 ELEMENTOS
+    tamanho_lista(p2, &tamP2);//JA QUE O LIMITE SAO 20 ELEMENTOS
+
+    if(tamP + tamP2 > 20) return 0;
+
+    int i;
+    for(i = 0; i < p->Fim; i++){//COMECO INSERINDO OS ELEMENTOS DA LISTA P EM P3
+        insere_elem(p3,p->num[i]);
+    }
+    for(i = 0; i < p2->Fim; i++){//DEPOIS INSIRO OS ELEMENTOS DA LISTA P2 EM P3
+        insere_elem(p3,p2->num[i]);
+    }
+
+    return 1;
+
+}
+
+void libera(Reais p, Reais p2, Reais p3){
+
+    if(p != NULL){free(p);p = NULL;}
+    if(p2 != NULL){free(p2);p2 = NULL;}
+    if(p3 != NULL){free(p3);p3 = NULL;}
+
+    //LIBERO AS 3 LISTAS E APONTO PARA NULL
+
+}
---
+++ 1/main.c
@@ -0,0 +1,183 @@
+#include <stdio.h>
+#include "function.h"
+
+Reais p, p2, p3;
+int op = 0, ini = 0;
+float elem;
+
+void menu(){
+
+    printf("[1] Criar Lista\n[2] Inserir Elemento\n");
+    printf("[3] Remover Elemento\n[4] Imprimir Lista\n");
+    printf("[5] Esvaziar Lista\n[6] Remover Numeros Impares\n");
+    printf("[7] Obter Maior Elemento\n[8] Obter Tamanho da Lista\n");
+    printf("[9] Gerar Lista Invertida\n[10] Concatenar Listas\n[11] Sair\n");
+    printf("\nDigite a opcao desejada: ");
+
+}
+
+
+void imprimir_lista(Reais p){
+
+    if(p != NULL){
+        int i = 0;
+        printf("\n{ ");
+        while(get_pos(p,i,&elem) != 0){
+            printf("%.2f ", elem);
+            i++;
+        }
+        printf("}\n\n");
+    }
+
+}
+
+int main(){
+
+    menu();
+
+    while(scanf("%d", &op) && op != 11){
+
+        if(op < 1 || op > 11){ // RECONHECE QND DIGITA UM NUMERO QUE NAO TEM FUNCAO
+            printf("\n### Operacao Invalida ###\n\n");
+            menu();
+            continue;
+        }
+
+        if(ini == 0 && op != 1){ // NAO DEIXA FAZER ALGO COM A LISTA SE ELA NAO EXISTIR
+            printf("\n### Lista NAO Inicializada ###\n\n");
+            menu();
+            continue;
+        }
+
+        if(ini == 1 && op == 1){ // IMPEDIRA QUE APAGUE A LISTA USANDO O CRIA LISTA, JA QUE TEM O COMANDO DE ESVAZIAR
+            printf("\n### A Lista Ja Foi Inicializada ###\n");
+            printf("### Caso deseje Esvaziar A Lista, Tecle 5 ###\n\n");
+            menu();
+            continue;
+        }
+
+        if(op == 1){
+            p = cria_lista();
+            if(p == 0) printf("\n### Falha ao Criar Lista ###\n\n");
+            else{
+                printf("\n### Lista Criada com Sucesso ###\n\n");
+                ini = 1;
+            }
+            menu();
+        }
+
+        if(op == 2){
+            printf("\n### A Lista possui os seguintes elementos: ###\n\n");
+            imprimir_lista(p);
+            printf("\nDigite o Elemento: ");
+            scanf("%f", &elem);
+            if(insere_elem(p, elem) == 0){
+                printf("\n### Falha ao Inserir Elemento ###\n\n");
+                menu();
+                continue;
+            }
+
+            printf("\n###Elemento Inserido com Sucesso ###\n\n");
+            menu();
+        }
+
+        if(op == 3){
+            printf("\n### A Lista possui os seguintes elementos: ###\n\n");
+            imprimir_lista(p);
+            printf("\nDigite o Elemento: ");
+            scanf("%f", &elem);
+            if(remove_elem(p, elem) == 0){
+                printf("\n### Falha ao Remover Elemento ###\n\n");
+                menu();
+                continue;
+            }
+
+            printf("\n### Elemento Removido com Sucesso ###\n\n");
+            menu();
+        }
+
+        if(op == 4){
+            imprimir_lista(p);
+            menu();
+        }
+
+        if(op == 5){
+            if(esvazia_lista(p) == 0){
+                printf("\n### Falha ao Esvaziar Lista ###\n\n");
+                menu();
+                continue;
+            }
+
+            printf("\n### Lista Esvaziada com Sucesso ###\n\n");
+            printf("### A Nova Lista eh: ###\n");
+            imprimir_lista(p);
+            menu();
+        }
+
+        if(op == 6){
+            if(remove_impares(p) == 0){
+                printf("\n### Falha ao Remover Numeros Impares ###\n\n");
+                menu();
+                continue;
+            }
+
+            printf("\n### Numeros Impares Removidos com Sucesso ###\n\n");
+            printf("### A Nova Lista eh: ###\n");
+            imprimir_lista(p);
+            menu();
+        }
+
+        if(op == 7){
+            if(maior_elemento(p,&elem) == 0){
+                printf("\n### Falha ao Obter Maior Elemento ###\n\n");
+                menu();
+                continue;
+            }
+            printf("\n### O Maior Elemento da Lista eh %.2f ###\n\n", elem);
+            menu();
+        }
+
+        if(op == 8){
+            if(tamanho_lista(p,&elem) == 0){
+                printf("\n### Falha ao Obter Tamanho da Lista ###\n\n");
+                menu();
+                continue;
+            }
+            printf("\n### O Tamanho da Lista eh de %.f Elemento(s) ###\n\n", elem);
+            menu();
+        }
+
+        if(op == 9){
+            p2 = cria_lista();
+            if(lista_invertida(p,p2) == 0){
+                printf("\n### Falha ao Gerar Lista Invertida ###\n\n");
+                menu();
+                continue;
+            }
+            printf("\n### Lista Invertida Gerada com Sucesso ###\n\n");
+            imprimir_lista(p2);
+            printf("\n### Lista Invertida Gerada com Sucesso ###\n\n");
+            menu();
+        }
+
+        if(op == 10){
+            p3 = cria_lista();
+            if(concatena_listas(p,p2,p3) == 0){
+                printf("\n### Falha ao concatenar Listas ###\n\n");
+                menu();
+                continue;
+            }
+            printf("\n### Listas Concatenadas com Sucesso ###\n\n");
+            imprimir_lista(p3);
+            printf("\n### Listas Concatenadas com Sucesso ###\n\n");
+            menu();
+        }
+
+    }
+
+    printf("\n### Fim Do Programa ###\n\n");
+
+    libera(p,p2,p3); // IRA DAR FREE NAS LISTAS INICIALIZADAS E APONTAR PARA NULL
+
+    return 0;
+}
