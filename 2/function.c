18b9331d6d9c5e1a509f1756f3dbee020a00a797
artfrc <80659617+artfrc@users.noreply.github.com>
2021-04-10 13:30:07 -0300

    Confiram se está certo
    Segunda Questão do Trabalho de AED

---
+++ 2/function.c
@@ -0,0 +1,191 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function.h"
+#define MAX 20
+
+struct CARACTER{
+
+  char elem[MAX];
+  int Fim;
+
+};
+
+CHAR cria_lista(){
+
+    CHAR p;
+    p = (CHAR)malloc(sizeof(struct CARACTER));
+
+    if(p != NULL) p->Fim = 0;
+
+    return p;
+
+}
+
+int lista_vazia(CHAR p){
+
+    if(p == NULL || p->Fim > 0) return 0;
+    else                        return 1;
+
+}
+
+int lista_cheia(CHAR p){
+
+    if(p == NULL || p->Fim != MAX) return 0;
+    else                           return 1;
+
+}
+
+int insere_ord(CHAR p, char N){
+
+    if(p == NULL || lista_cheia(p) == 1) return 0;
+
+//SE A LISTA NAO ESTIVER CHEIA, VAI PROCURAR UM LUGAR PARA INSERIR
+    if(lista_vazia(p) == 1){//SE NAO TIVER ELEMENTO, INSERE NA PRIMEIRA POSICAO E PRONTO
+        p->elem[p->Fim] = N;
+        p->Fim++;
+        return 1;
+    }
+    int i, j;
+    for(i = 0; i < p->Fim; i++){//SE TIVER ELEMENTOS, PROCURA UM ELEMENTO DA LISTA MENOR
+        if(p->elem[i] <= N) break;//PARA PODER INSERIR EM ORDEM DECRESCENTE
+    }
+    for(j = p->Fim; j > i; j--){//MOVENDO TODOS OS ELEMENTOS, PARA PODER INSERIR O NOVO ELEMENTO
+        p->elem[j] = p->elem[j-1];
+    }
+    p->elem[j] = N;//INSERCAO DO NOVO ELEMENTO
+    p->Fim++;//ACRESCENTA MAIS UM EM FIM, PARA DIZER Q O ELEMENTO FOI ACRESCENTADO
+
+    return 1;
+
+}
+
+int remove_elem(CHAR p, char N){
+
+    if(p == NULL || lista_vazia(p) == 1) return 0;
+
+
+// SE O ELEMENTO QUE DESEJA SER REMOVIDO FOR O ULTIMO DA LISTA, SÓ DA UM FIM -1
+// PARA DIZER QUE AQUELE ELEMENTO NAO PERTENCE MAIS A LISTA
+    else if(p->elem[p->Fim-1] == N){p->Fim--; return 1;}
+
+//SE NAO FOR O ULTIMO, ELE PROCURA ATE ACHAR OU NAO O ELEMENTO
+    else{
+        int i;
+        for(i = 0; i < p->Fim; i++){
+            if(p->elem[i] == N) break; //SE ACHOU, ENTAO PAUSA E IREMOS REMOVE-LO ADIANTE
+        }
+
+        if(i == p->Fim) return 0; //SE NAO ACHOU, RETORNA ZERO
+
+        else{ //REMOVER ELEMENTO ENCONTRADO
+            for(i += 1; i < p->Fim; i++){ //COMECO NA POSICAO DO ELEMENTO + 1
+                p->elem[i-1] = p->elem[i];//PEGO ESSE ELEMENTO E COLOCO NA POSICAO ANTERIOR
+            }                           //FACO ISSO ATE O FINAL
+            p->Fim--;               //E DOU UM FIM - 1 PARA DIZER QUE REMOVI UM ELEMENTO
+            return 1;
+        }
+    }
+
+}
+
+int get_pos(CHAR p, int pos, char *elemento){
+
+    if(p == NULL || lista_vazia(p) == 1 || p->Fim <= pos) return 0;
+
+    //JA FACO A VERIFICACAO ANTES SE A POSICAO SOLICITADA EH VALIDA
+    //SE FOR, SO PEGO O ELEMENTO DAQUELA POSICAO E PASSO POR REFERENCIA
+    *elemento = p->elem[pos];
+
+    return 1;
+
+}
+
+int esvazia_lista(CHAR p){
+
+    if(p == NULL) return 0;
+
+    //SIMPLESMENTE COLOCO FIM = 0 PARA DIZER QUE A LISTA ESTA VAZIA
+    //O QUE FICOU LA SERA LIXO
+
+    p->Fim = 0;
+
+    return 1;
+
+}
+
+int remove_pares(CHAR p){
+
+    if(p == NULL || lista_vazia(p) == 1) return 0;
+
+    int i;
+    for(i = 0; i < p->Fim; i++){
+        if(p->elem[i] % 2 == 0){
+            remove_elem(p, p->elem[i]);
+            i--; //COMANDO NECESSARIO, JA QUE IREI REMOVER O ELEMENTO SE FOR PAR, E O PROXIMO Q IREI VERIFICAR
+        }       //IRA VIR PARA ESSA POSICAO. SEM ESSE COMANDO, EU NAO FARIA A VERIFICACAO DESSE PROXIMO ELEMENTO
+    }
+
+    return 1;
+
+}
+
+int menor_elemento(CHAR p, char *elemento){
+
+    if(p == NULL || lista_vazia(p) == 1) return 0;
+
+    int i;
+    *elemento = p->elem[0]; //COLOCO PARA O PRIMEIRO ELEMENTO SER O MENOR
+    for(i = 1; i < p->Fim; i++){//DPS VOU FAZENDO A VERIFICACAO SE EXISTE OUTRO MENOR
+        if(p->elem[i] < *elemento) *elemento = p->elem[i];
+    }
+    //O MENOR ELEMENTO EH PASSADO POR REFERENCIA
+
+    return 1;
+
+}
+
+int tamanho_lista(CHAR p, int *tam){
+
+    if(p == NULL) return 0;
+
+    *tam = p->Fim;
+
+    return 1;
+
+}
+
+int intercala_listas(CHAR p, CHAR p2, CHAR p3){
+
+    if(p == NULL || p2 == NULL || p3 == NULL) return 0;
+
+    int tamP, tamP2;
+    tamanho_lista(p, &tamP);//A SOMA DO TAMANHO DAS 2 LISTAS PRECISA SER NO MAXIMO 20 ELEMENTOS
+    tamanho_lista(p2, &tamP2);//JA QUE O LIMITE SAO 20 ELEMENTOS
+
+    if(tamP + tamP2 > 20) return 0;
+
+    int i;
+    for(i = 0; i < p->Fim; i++){//COMECO INSERINDO OS ELEMENTOS DA LISTA P EM P3
+        insere_ord(p3,p->elem[i]);
+    }
+    for(i = 0; i < p2->Fim; i++){//DEPOIS INSIRO OS ELEMENTOS DA LISTA P2 EM P3
+        insere_ord(p3,p2->elem[i]);
+    }
+    /* OS ELEMENTOS DA LISTA 1 SAO INSERIDOS ORDENADOS EM ORDEM DESCRECENTE, JÁ QUE A LISTA
+    JÁ ESTÁ ORDENADA ASSIM, E TBM ESTÁ PASSANDO PELA FUNCAO INSERE_ORD!
+    OS ELEMENTOS DA LISTA 2 TBM SAO INSERIDOS EM ORDEM DESCRESCENTE, MESMO COM OS ELEMENTOS
+    DA LISTA 1 JÁ INSERIDOS. NOTE QUE TBM SAO INSERIDOS PELA FUNCAO INSERE_ORD */
+
+    return 1;
+
+}
+
+void libera(CHAR p, CHAR p2, CHAR p3){
+
+    if(p != NULL){free(p);p = NULL;}
+    if(p2 != NULL){free(p2);p2 = NULL;}
+    if(p3 != NULL){free(p3);p3 = NULL;}
+
+    //LIBERO AS 3 LISTAS E APONTO PARA NULL
+
+}
---
+++ 2/main.c
@@ -0,0 +1,193 @@
+#include <stdio.h>
+#include "function.h"
+
+CHAR p, p2, p3;
+int op = 0, ini = 0, tam;
+char elem;
+
+void menu(){
+
+    printf("[1] Criar Lista\n[2] Inserir Elemento\n");
+    printf("[3] Remover Elemento\n[4] Imprimir Lista\n");
+    printf("[5] Esvaziar Lista\n[6] Remover Caracteres Pares\n");
+    printf("[7] Obter Menor Elemento\n[8] Obter Tamanho da Lista\n");
+    printf("[9] Intercalar Listas\n[10] Sair\n");
+    printf("\nDigite a opcao desejada: ");
+
+}
+
+
+void imprimir_lista(CHAR p){
+
+    if(p != NULL){
+        int i = 0;
+        printf("\n{ ");
+        while(get_pos(p,i,&elem) != 0){//COMECA NA POSICAO ZERO E VAI IMPRIMIR ATE O GET FALHAR
+            printf("%c ", elem);
+            i++;
+        }
+        printf("}\n\n");
+    }
+
+}
+
+int main(){
+
+    menu();
+
+    while(scanf("%d", &op) && op != 10){
+
+        setbuf(stdin, NULL);
+
+        if(op < 1 || op > 10){ // RECONHECE QND DIGITA UM NUMERO QUE NAO TEM FUNCAO
+            printf("\n### Operacao Invalida ###\n\n");
+            menu();
+            continue;
+        }
+
+        if(ini == 0 && op != 1){ // NAO DEIXA FAZER ALGO COM A LISTA 1 SE ELA NAO EXISTIR
+            printf("\n### Lista NAO Inicializada ###\n\n");
+            menu();
+            continue;
+        }
+
+        if(ini == 1 && op == 1){ // IMPEDIRA QUE APAGUE A LISTA 1 USANDO O CRIA LISTA, JA QUE TEM O COMANDO DE ESVAZIAR
+            printf("\n### A Lista Ja Foi Inicializada ###\n");
+            printf("### Caso deseje Esvaziar A Lista, Tecle 5 ###\n\n");
+            menu();
+            continue;
+        }
+
+        if(op == 1){
+            p = cria_lista();
+            if(p == 0) printf("\n### Falha ao Criar Lista ###\n\n");
+            else{
+                printf("\n### Lista Criada com Sucesso ###\n\n");
+                ini = 1;
+            }
+            menu();
+        }
+
+        if(op == 2){
+            printf("\n### A Lista possui os seguintes elementos: ###\n\n");
+            imprimir_lista(p);
+            printf("\nDigite o Elemento: ");
+            scanf("%c", &elem);
+            if(insere_ord(p, elem) == 0){
+                printf("\n### Falha ao Inserir Elemento ###\n\n");
+                menu();
+                continue;
+            }
+
+            printf("\n### Elemento Inserido com Sucesso ###\n\n");
+            menu();
+        }
+
+        if(op == 3){
+            printf("\n### A Lista possui os seguintes elementos: ###\n\n");
+            imprimir_lista(p);
+            printf("\nDigite o Elemento: ");
+            scanf("%c", &elem);
+            if(remove_elem(p, elem) == 0){
+                printf("\n### Falha ao Remover Elemento ###\n\n");
+                menu();
+                continue;
+            }
+
+            printf("\n### Elemento Removido com Sucesso ###\n\n");
+            menu();
+        }
+
+        if(op == 4){
+            imprimir_lista(p);
+            menu();
+        }
+
+        if(op == 5){
+            if(esvazia_lista(p) == 0){
+                printf("\n### Falha ao Esvaziar Lista ###\n\n");
+                menu();
+                continue;
+            }
+
+            printf("\n### Lista Esvaziada com Sucesso ###\n\n");
+            printf("### A Nova Lista eh: ###\n");
+            imprimir_lista(p);
+            menu();
+        }
+
+        if(op == 6){
+            if(remove_pares(p) == 0){
+                printf("\n### Falha ao Remover Caracteres Pares ###\n\n");
+                menu();
+                continue;
+            }
+
+            printf("\n### Caracteres Pares Removidos com Sucesso ###\n\n");
+            printf("### A Nova Lista eh: ###\n");
+            imprimir_lista(p);
+            menu();
+        }
+
+        if(op == 7){
+            if(menor_elemento(p,&elem) == 0){
+                printf("\n### Falha ao Obter Menor Elemento ###\n\n");
+                menu();
+                continue;
+            }
+            printf("\n### O Menor Elemento da Lista eh '%c' ###\n\n", elem);
+            menu();
+        }
+
+        if(op == 8){
+            if(tamanho_lista(p,&tam) == 0){
+                printf("\n### Falha ao Obter Tamanho da Lista ###\n\n");
+                menu();
+                continue;
+            }
+            printf("\n### O Tamanho da Lista eh de %d Elemento(s) ###\n\n", tam);
+            menu();
+        }
+
+        if(op == 9){
+            p2 = cria_lista();//INICIALIZA LISTA P2
+            p3 = cria_lista();//INICIALIZA LISTA P3
+
+            printf("\n### INSIRA ELEMENTOS NA LISTA 2 ###\n\n");
+            printf("### Caso NAO queira Inserir Mais Elementos, Tecle SPACE e de ENTER ###\n");
+            printf("\nDigite o elemento desejado: ");
+            while(scanf("%c", &elem) && elem != ' '){//INSERIR ELEMENTOS NA LISTA P2
+                setbuf(stdin, NULL);
+                if(insere_ord(p2, elem) == 0){
+                   printf("\n### Erro ao Inserir Elemento ###\n\n");
+                   continue;
+                }
+                printf("\n### Elemento Inserido com Sucesso ###\n");
+                printf("\n### Caso NAO queira Inserir Mais Elementos, Tecle SPACE e de ENTER ###\n");
+                printf("\nDigite o elemento desejado: ");
+            }
+
+            if(intercala_listas(p,p2,p3) == 0){//INSERE OS ELEMENTOS DE P E P2 EM P3
+                printf("\n### Falha ao Gerar Lista Intercalada ###\n\n");
+                menu();
+                continue;
+            }
+            printf("\n### Lista Intercalada Gerada com Sucesso ###\n\n");
+            printf("\n### Lista 1 ###\n");
+            imprimir_lista(p);
+            printf("\n### Lista 2 ###\n");
+            imprimir_lista(p2);
+            printf("\n### Lista 3 (Intercalada) ###\n");
+            imprimir_lista(p3);
+            printf("\n### Lista Intercalada Gerada com Sucesso ###\n\n");
+            menu();
+        }
+
+    }
+
+    printf("\n### Fim Do Programa ###\n\n");
+
+    libera(p,p2,p3); // IRA DAR FREE NAS LISTAS INICIALIZADAS E APONTAR PARA NULL
+
+    return 0;
+}
