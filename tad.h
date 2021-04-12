bb31100d0eefedc9a9f16eb19fdb7a87acaa5519
Renata Cristina <renatinha.8@hotmail.com>
2021-04-11 22:06:22 -0300

    Update tad.h

--- tad.h
+++ tad.h
@@ -4,7 +4,7 @@ Lista criar_lista();
 int lista_vazia(Lista lista);
 int insere_ord(Lista *lista, int elemento);
 int remove_ord(Lista *lista, int elemento);
-int obtem_valor(Lista *lista, int *elemento, int posicao);
+//int obtem_valor(Lista *lista, int *elemento, int posicao);
 int esvaziar_lista(Lista *lista);
 int remove_pos(Lista *lista, int pos, int *elemento);
 int listas_iguais(Lista *lista_1, Lista *lista_2);
