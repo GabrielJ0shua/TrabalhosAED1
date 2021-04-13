7fab2ca5d595fb3670b8e5e0a8a1333dbdd1b6ef
Alexandre Justo <38166703+LeppyT@users.noreply.github.com>
2021-04-13 00:38:19 -0300

    Mains feitr

---
+++ encadeamento_ciclico/ciclico.h
@@ -0,0 +1,13 @@
+#ifndef CICLICO_H_INCLUDED
+#define CICLICO_H_INCLUDED
+#include <stdlib.h>
+#include <stdio.h>
+typedef struct no * Lista;
+Lista cria_lista();
+int remove_inicio(Lista *lst, char *elem);
+int insere_final(Lista *lst, char elem);
+int insere_inicio(Lista *lst, char elem);
+int tamanho_lista(Lista * lst);
+int remove_final(Lista *lst, char *elem);
+char remove_pos(Lista *lst, int pos);
+#endif // CICLICO_H_INCLUDED
