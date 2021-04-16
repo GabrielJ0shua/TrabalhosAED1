81c79cea323eb1d66a3622ba3e79657394d1f4ca
Renata Cristina <renatinha.8@hotmail.com>
2021-04-16 13:49:36 -0300

    03
    PRONTO

---
+++ 03/tad.h
@@ -0,0 +1,13 @@
+typedef struct no *Lista;
+
+Lista criar_lista();
+int lista_vazia(Lista lista);
+int insere_ord(Lista *lista, int elemento);
+int remove_ord(Lista *lista, int elemento);
+int obtem_valor(Lista *lista, int *elemento, int pos);
+int esvaziar_lista(Lista *lista);
+int remove_pos(Lista *lista, int pos, int *elemento);
+int maior_valor(Lista *lista, int *elemento);
+int tamanho_lista(Lista *lista);
+int intercalar_listas(Lista *lista_1, Lista *lista_2, Lista *lista_3);
+int listas_iguais(Lista *lista_1, Lista *lista_2, int tam1, int tam2);
