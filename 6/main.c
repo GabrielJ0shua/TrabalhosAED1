651bf3c5e81fb58a61240bff3f676e5c1a9d6af4
Alexandre Justo <38166703+LeppyT@users.noreply.github.com>
2021-04-19 17:50:20 -0300

    Main da 6 em aplicativo

--- 6/main.c
+++ 6/main.c
@@ -1,28 +1,111 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "duplo.h"
+
 int main()
 {
-    Lista lst;
-    lst = cria_lista;
+    char tester;
+    Lista lst, lst2;
+    lst = cria_lista();
+    char input;
+    int posicao, tam;
+    int escolha = 0;
+    while(escolha!=11)
+    {
+        printf("Manipulador de lista de int: \n");
+        printf("[1] Criar lista \n");
+        printf("[2] Apagar lista \n");
+        printf("[3] Inserir \n");
+        printf("[4] Remover\n");
+        printf("[5] Remover todos\n");
+        printf("[6] Imprimir Lista\n");
+        printf("[7] Tamanho da Lista\n");
+        printf("[8] Remover maior\n");
+        printf("[9] Inverter\n");
+        printf("[10] Primos\n");
+        printf("[11] Sair\n");
+        scanf("%d", &escolha);
+        switch (escolha)
+        {
+            case 1:
+                if(lst == NULL)
+                    lst = cria_lista();
+                else
+                    printf("Lista ja existe \n");
+            break;
 
-    insere_elem(&lst, 3);
-    insere_elem(&lst, 5);
-    insere_elem(&lst, 9);
-    imprime_lista(lst);
+            case 2:
+                if(lst == NULL)
+                    printf("Lista nao existe \n");
+                else
+                        apaga_lista(&lst);
+            break;
+            case 3:
+                printf("Digite o caractere a ser inserido: \n");
+                scanf("%d", &input);
+                insere_elemento(&lst, input);
+            break;
 
-    printf("maior valor removido: %d", remove_maior(*lst));
-    imprime_lista(lst);
+            case 4:
+                 scanf("%d", &input);
+                 if (remove_elemento(&lst, input)==0)
+                 {
+                     printf("Falha \n");
+                 }
+            break;
 
-    insere_elem(&lst, -13);
-    insere_elem(&lst, 13);
-    Lista lista_2 = inverte(&lst);
-    imprime_lista(lista_2);
+            case 5:
+                scanf("%d", &input);
+                 if (remove_todos(&lst, &input)==0)
+                 {
+                     printf("Falha \n");
+                 }
+                 else
+                    printf("Elemento %c removido \n", input);
+            break;
 
-    lista_2 = primos(&lst);
-    imprime_lista(lista_2);
-    esvaziar(&lista_2);
-    esvaziar(&lst);
-    imprime_lista(lista_2);
+            case 6:
+                if(lista_vazia(lst)!=1)
+                {
+                    imprime_lista(lst);
+                }
+                else
+                    printf("Falha, lista vazia \n");
+            break;
+
+            case 7:
+                tam = tamanho_lista(&lst);
+                printf("%d", tam);
+            break;
+            case 8:
+                if (remove_maior(&lst, &input)==0)
+                 {
+                     printf("Falha \n");
+                 }
+                 else
+                    printf("Elemento %d removido \n", input);
+            break;
+            case 9:
+                scanf("%d", &posicao);
+                tester = remove_pos(&lst, posicao);
+                if (tester==0)
+                 {
+                     printf("Falha ao encontrar a posicao \n");
+                 }
+                 else
+                    printf("Elemento %d removido \n", tester);
+            break;
+
+            case 10:
+                lst2 = primos(lst);
+                break;
+            case 11:
+                apaga_lista(&lst);
+                break;
+            default:
+                printf("Escolha invalida");
+        }
+    }
     return 0;
 }
+
