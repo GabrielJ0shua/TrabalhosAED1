3fc7dc83190c0de7824a4bdef6fa4d453e37891c
Gabriel Joshua <gjoshuacalixto@outlook.com>
2021-05-17 20:23:25 -0300

    Delete lista-12-04-2021AED1Pres directory

--- lista-12-04-2021AED1Pres/1/tad.h
+++
@@ -1,8 +0,0 @@
-typedef struct caixa cad;
-typedef struct caixa* lista;
-lista *cria(void);
-void libera (lista *li);
-int ord(lista *li,int l);
-int rem(lista *li,int b);
-int pos(lista *li,int b, int *aux);
-int imprime(lista *li, int i, int *aux);
